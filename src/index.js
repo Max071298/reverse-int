a55b6c83a1b8779210791ddf5fe9f9acafd394d3
Max071298 <102859892+Max071298@users.noreply.github.com>
2023-07-31 18:45:40 +0300

    fix: add solution of task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let nStringified =
+        n.toString()[0] === "-" ? n.toString().slice(1) : n.toString();
+    return Number(`${nStringified[2]}${nStringified[1]}${nStringified[0]}`);
+};
